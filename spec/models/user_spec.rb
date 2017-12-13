2a5f86a86da6816da66e6cf5cf4aa5009704c1c1
John Schmid <jschmid808@gmail.com>
2017-12-13 15:16:08 -0500

    added posts model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe User, type: :model do
 	  	expect(@user).to be_valid
 	  end
 
-	  it "cannot be created without first_name, last_name" do
+	  it "cannot be created w/o first_name, last_name" do
 	  	@user.first_name = nil
 	  	@user.last_name = nil
 	  	expect(@user).to_not be_valid
