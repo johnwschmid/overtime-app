009a34eddf751ac5f2c38287a2da43f86d89079d
John Schmid <jschmid808@gmail.com>
2017-12-22 16:04:28 -0500

    Added posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,33 @@ describe 'navigate' do
 			expect(page).to have_content(/Posts/)
 		end
 	end
+
+	describe 'creation' do
+
+		before do
+	  	# @user = User.create(email: "test@test.test", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+	  	# login_as(user, :scope => :user)
+			visit new_post_path
+		end
+
+		it 'has a new form that can be reached' do
+			expect(page.status_code).to eq(200)
+		end
+
+		it 'form can create posts' do
+			fill_in 'post[date]', with: Date.today
+			fill_in 'post[rationale]', with: 'Some Rationale'
+			click_on 'Save'
+
+			expect(page).to have_content("Some Rationale")
+		end
+
+		# it 'will have a user associated with post'
+		# 	fill_in 'post[date]', with: Date.today
+		# 	fill_in 'post[rationale]', with: 'User Association'
+		# 	click_on 'Save'
+
+		# 	expect(User.last.posts.last.rationale).to eq("User Assocation")
+		# end
+	end
 end
